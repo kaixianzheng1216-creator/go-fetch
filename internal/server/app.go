3c7399ed39c6c1fddb0bd13e983a1edac3012d0a
kaixianzheng <kaixianzheng1216@gmail.com>
2026-05-14 15:26:36 +0800

    config(sessions): 更新会话配置并添加安全中间件

--- internal/server/app.go
+++ internal/server/app.go
@@ -35,10 +35,15 @@ func newSessionManager(dataStore *store.Store) *scs.SessionManager {
 
 	sessions.Store = pgxstore.NewWithConfig(dataStore.Pool(), pgxstore.Config{
 		TableName:       "app_sessions",
-		CleanUpInterval: 5 * time.Minute,
+		CleanUpInterval: 10 * time.Minute,
 	})
 
 	sessions.Cookie.Name = sessionCookieName
+	sessions.Cookie.Secure = true
+	sessions.Cookie.HttpOnly = true
+	sessions.Cookie.SameSite = http.SameSiteLaxMode
+
+	sessions.Lifetime = 24 * time.Hour
 
 	return sessions
 }
@@ -46,7 +51,11 @@ func newSessionManager(dataStore *store.Store) *scs.SessionManager {
 func (a *App) Routes() http.Handler {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(30 * time.Second))
 	r.Use(a.sessions.LoadAndSave)
 
 	api := humachi.New(r, humaConfig())
